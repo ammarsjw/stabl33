dbcc7d40da999634f59f9b5685ef3b0158329447
ammarsjw <ammarsjw@gmail.com>
2022-08-23 13:24:05 +0500

    added graphs

---
+++ GraphTreasury/src/mapping.ts
@@ -0,0 +1,54 @@
+import { Bytes, BigInt } from "@graphprotocol/graph-ts"
+
+import {
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Rate as RateEvent,
+  UpdatedPermission as UpdatedPermissionEvent,
+  UpdatedReservedToken as UpdatedReservedTokenEvent 
+} from "../generated/Treasury/Treasury"
+
+import {
+  OwnershipTransferred,
+  Rate,
+  UpdatedPermission,
+  UpdatedReservedToken
+} from "../generated/schema"
+
+import { loadOrCreateTransaction } from "./utils/Transactions"
+
+export function handleOwnershipTransferred(event: OwnershipTransferredEvent): void {
+  let transaction = loadOrCreateTransaction(event.transaction, event.block);
+  let entity = new OwnershipTransferred(transaction.id)
+  entity.transaction = transaction.id
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+  entity.save()
+}
+
+export function handleUpdatedPermission(event: UpdatedPermissionEvent): void {
+  let transaction = loadOrCreateTransaction(event.transaction, event.block);
+  let entity = new UpdatedPermission(transaction.id)
+  entity.transaction = transaction.id
+  entity.contractAddress = event.params.contractAddress
+  entity.state = event.params.state
+  entity.save()
+}
+
+export function handleUpdatedReservedToken(event: UpdatedReservedTokenEvent): void {
+  let transaction = loadOrCreateTransaction(event.transaction, event.block);
+  let entity = new UpdatedReservedToken(transaction.id)
+  entity.transaction = transaction.id
+  entity.token = event.params.token
+  entity.decimals = event.params.decimals
+  entity.state = event.params.state
+  entity.save()
+}
+
+export function handleRate(event: RateEvent): void {
+  let transaction = loadOrCreateTransaction(event.transaction, event.block);
+  let entity = new Rate(event.params.blockTimestampLast.toString())
+  entity.transaction = transaction.id
+  entity.rate = event.params.rate
+  entity.blockTimestampLast = event.params.blockTimestampLast
+  entity.save()
+}
---
+++ GraphTreasury/src/utils/transactions.ts
@@ -0,0 +1,22 @@
+import { ethereum, log } from '@graphprotocol/graph-ts'
+// schema imports
+import { Transaction } from '../../generated/schema'
+
+export function loadOrCreateTransaction(eth_transaction: ethereum.Transaction, eth_block: ethereum.Block): Transaction {
+  log.debug('loadOrCreateTransaction {}', [eth_transaction.hash.toHex()])
+  let transaction = Transaction.load(eth_transaction.hash.toHex())
+  if (transaction == null) {
+    transaction = new Transaction(eth_transaction.hash.toHex())
+    transaction.timestamp = eth_block.timestamp
+    transaction.blockNumber = eth_block.number
+    transaction.blockHash = eth_block.hash
+    transaction.from = eth_transaction.from
+    transaction.to = eth_transaction.to
+    transaction.value = eth_transaction.value
+    transaction.gasPrice = eth_transaction.gasPrice
+    transaction.save()
+    log.debug('new transaction created {}', [eth_transaction.hash.toHex()])
+  }
+  
+  return transaction as Transaction
+}
