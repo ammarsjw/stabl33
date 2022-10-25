d3c46053b6710088281ba4cbdce0314942627efc
ammarsjw <ammarsjw@gmail.com>
2022-10-25 13:15:31 +0500

    updated ROI graph

--- GraphROI/generated/ROI/ROI.ts
+++ GraphROI/generated/ROI/ROI.ts
@@ -244,38 +244,6 @@ export class ROI extends ethereum.SmartContract {
     return ethereum.CallResult.fromValue(value[0].toBigInt());
   }
 
-  getStakingAPRIndex(param0: BigInt, param1: BigInt): BigInt {
-    let result = super.call(
-      "getStakingAPRIndex",
-      "getStakingAPRIndex(uint256,uint256):(uint256)",
-      [
-        ethereum.Value.fromUnsignedBigInt(param0),
-        ethereum.Value.fromUnsignedBigInt(param1)
-      ]
-    );
-
-    return result[0].toBigInt();
-  }
-
-  try_getStakingAPRIndex(
-    param0: BigInt,
-    param1: BigInt
-  ): ethereum.CallResult<BigInt> {
-    let result = super.tryCall(
-      "getStakingAPRIndex",
-      "getStakingAPRIndex(uint256,uint256):(uint256)",
-      [
-        ethereum.Value.fromUnsignedBigInt(param0),
-        ethereum.Value.fromUnsignedBigInt(param1)
-      ]
-    );
-    if (result.reverted) {
-      return new ethereum.CallResult();
-    }
-    let value = result.value;
-    return ethereum.CallResult.fromValue(value[0].toBigInt());
-  }
-
   getTotalRewardDistributed(): BigInt {
     let result = super.call(
       "getTotalRewardDistributed",
