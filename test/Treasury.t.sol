47bd28358eb3a66442c203806a3fc71167642329
ammarsjw <ammarsjw@gmail.com>
2023-06-12 18:54:19 +0500

    test init

---
+++ test/Treasury.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import "forge-std/Test.sol";
+
+import  "../contracts/Treasury.sol";
+
+contract TreasuryTest is Test {
+
+    // Invoked before each test
+    function setUp() public {
+    }
+}
