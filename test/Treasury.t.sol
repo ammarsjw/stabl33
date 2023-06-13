a90c258379c5f5b0fe36601d178be97ee5579b9c
ammarsjw <ammarsjw@gmail.com>
2023-06-13 11:27:41 +0500

    cc

--- test/Treasury.t.sol
+++ test/Treasury.t.sol
@@ -4,11 +4,19 @@ pragma solidity 0.8.19;
 
 import "forge-std/Test.sol";
 
-import  "../contracts/Treasury.sol";
+import "../contracts/Treasury.sol";
+import "../contracts/ROI.sol";
+import "../contracts/Stabl3PublicSale.sol";
 
 contract TreasuryTest is Test {
 
-    // Invoked before each test
+    Treasury treasury;
+    ROI roi;
+    address hq;
+
+    Stabl3PublicSale publicSale;
+
+    /// @dev Invoked before each test.
     function setUp() public {
     }
 }
