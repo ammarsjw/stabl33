b227223e08eda4a0ccfb1b36dbe88c4f04a38b22
ammarsjw <ammarsjw@gmail.com>
2023-10-24 17:54:01 +0500

    rm

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,36 +3,39 @@ require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
 module.exports = {
-  solidity: {
-    compilers: [
-      {
-        version: '0.8.19',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-    ],
-  },
-  networks: {
-    mumbai: {
-      url: process.env.URL_MUMBAI,
-      accounts: [process.env.PRIVATE_KEY_MUMBAI],
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.19',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+        ],
     },
-    // goerli: {
-    //   url: process.env.URL,
-    //   accounts: [process.env.PRIVATE_KEY],
-    // },
-    // mainnet: {
-    //   url: process.env.URL_MAIN,
-    //   accounts: [process.env.PRIVATE_KEY_MAIN],
-    //   chainId: 1,
-    // },
-  },
-  etherscan: {
-    apiKey: 'WPJFDVCHRSQNBVUM5I5WCAW65C69BT6B3V' // mumbai
-    // apiKey: 'AYBZ53EN445WNPFP2IZ85RXRPB4FH5XBP7' // goerli
-  },
+    networks: {
+        mumbai: {
+            url: process.env.URL_MUMBAI,
+            accounts: [process.env.PRIVATE_KEY_MUMBAI],
+        },
+        goerli: {
+            url: process.env.URL_GOERLI,
+            accounts: [process.env.PRIVATE_KEY_GOERLI],
+        },
+        mainnet: {
+            url: process.env.URL_MAINNET,
+            accounts: [process.env.PRIVATE_KEY_MAINNET],
+            chainId: 1,
+        },
+    },
+    etherscan: {
+        apiKey: {
+            mumbai: process.env.BLOCK_EXPLORER_API_KEY_POLYGON,
+            goerli: process.env.BLOCK_EXPLORER_API_KEY_ETHEREUM,
+            mainnet: process.env.BLOCK_EXPLORER_API_KEY_ETHEREUM,
+        },
+    },
 };
